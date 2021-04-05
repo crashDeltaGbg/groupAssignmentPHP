2327bf619a4106331acc669cc2404f5767e2428f
Jon Rickard Yxelflod <rickard@yxelflod.se>
2021-04-05 13:47:34 +0200

    even further adjustments to upload.php

--- upload.php
+++ upload.php
@@ -7,26 +7,26 @@
   if (isset($_POST["submit"])) {
     $check = getimagesize($_FILES["file"]["tmp_name"]);
       if ($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
+        echo "File is an image - " . $check["mime"] . ". ";
         $uploadOk = 1;
       } else {
-        echo "File is not an image.";
+        echo "File is not an image. ";
         $uploadOk = 0;
       }
   }
 
   if (file_exists($target_file)) {
-    echo "The file already exists on the server.";
+    echo "The file already exists on the server. ";
     $uploadOk = 0;
   }
 
   if ($_FILES["file"]["size"] > 500000) {
-    echo "This image weighs too heavily.";
+    echo "This image weighs too heavily. ";
     $uploadOk = 0;
   }
 
   if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "svg" && $imageFileType != "gif") {
-    echo "You must only upload image type files, i.e. .jpg/.jpeg, .png, .svg or .gif.";
+    echo "You must only upload image type files, i.e. .jpg/.jpeg, .png, .svg or .gif. ";
     $uploadOk = 0;
   }
 
