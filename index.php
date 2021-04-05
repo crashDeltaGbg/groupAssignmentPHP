f8b61858b44cf7d9644d6d9dd0858fe772e18ceb
Jon Rickard Yxelflod <rickard@yxelflod.se>
2021-04-05 11:05:59 +0200

    index.php

--- index.php
+++ index.php
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Group Assignment PHP</title>
-</head>
-<body>
-  <header>
-  </header>
-
-  <main>
-  <form action="" method="get">
-  <label for="user">What's your name?</label>
-    <input type="text" name="user" id="user" placeholder="Your name">
-    <input type="submit" value="Submit">
-  </form>
-  <?php
-    $name = $_GET['user'];
-
-    echo "<p>Hello, " . $name . ", and welcome to our little PHP project!</p>";
-  ?>
-  </main>
-
-  <footer></footer>
-</body>
-</html>
-=======
 <?php
 // Slå på all felrapportering. Bra under utveckling, dåligt i produktion.
 ini_set('display_errors', 1);
@@ -67,4 +37,3 @@ echo "Hello, World!";
 ?>
 </body>
 </html>
->>>>>>> 56077daf2c87a0b3e042e45bc9514682460b694d
