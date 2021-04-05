18f23b9ed8413ee8e10b1d7452ca4d6b410147c5
Jon Rickard Yxelflod <rickard@yxelflod.se>
2021-04-05 11:03:06 +0200

    Index.php

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Group Assignment PHP</title>
+</head>
+<body>
+  <header>
+  </header>
 
-echo "Hello, World!";
+  <main>
+  <form action="" method="get">
+  <label for="user">What's your name?</label>
+    <input type="text" name="user" id="user" placeholder="Your name">
+    <input type="submit" value="Submit">
+  </form>
+  <?php
+    $name = $_GET['user'];
+
+    echo "<p>Hello, " . $name . ", and welcome to our little PHP project!</p>";
+  ?>
+  </main>
+
+  <footer></footer>
+</body>
+</html>
---
+++ personligtBidrag/bidragsrapportRickard.md
@@ -0,0 +1,9 @@
+#Bidragsrapport Rickard Yxelflod
+
+##Fredag 1/4
+
+Skapat github-repo och bjudit in de andra i gruppen
+
+##Måndag 5/4
+
+Skapat DB Zoo och importerat animals.sql
