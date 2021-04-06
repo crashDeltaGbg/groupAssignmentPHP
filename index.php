ae92a4a5be3af477defccddc990be3a9608cd506
Noel Andersson <noel.anderssonkohler@hotmail.com>
2021-04-06 10:06:37 +0200

    added search-function and a div to render results

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 // Slå på all felrapportering. Bra under utveckling, dåligt i produktion.
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -18,7 +17,7 @@ error_reporting(E_ALL);
 
 <main>
     <?php include "./includes/dbh.php"; ?>
-<form class="fullForm" enctype="multipart/form-data" action="index.php" method="POST">
+<form class="fullForm" action="index.php" method="POST">
     <label for="selCategory">Select Category</label>
     <select name="selCategory" id="category">
         <option value="category1"></option>
@@ -26,16 +25,39 @@ error_reporting(E_ALL);
         <option value="category3"></option>
         <option value="category4"></option>
     </select>
-
-    <label for="searchWord">Search Database</label>
+</form>
+<form class="search" action="index.php" method="POST">
+    <label for="searchWord">Search For Name or Category</label>
     <input type="text" name="searchWord">
     <input class="btn" type="submit" name="search_button" value="Sök"/>
 </form>
 
+<div class="resultsContainer">
+<!-- code for the search-function -->
 <?php
+    if (isset($_POST['search_button'])){
+        $search = $_POST['searchWord'];}
+    else {
+        $search = null;
+}
+// testing what is in the variable $search, will delete when finished
+var_dump($search);
 
-echo "Hello, World!";
+//PDO + query for name and category-search
+$query = "SELECT * FROM animals WHERE CONCAT(name, ' ', category) LIKE CONCAT('%', :search, '%')";
+$statement = $pdo->prepare($query, array(PDO::FETCH_ASSOC));
+$statement->execute(array(
+    ':search' => $_POST['searchWord'],
+));
+$result = $statement->fetchAll();
+
+// testing that query works
+foreach ($result as $key => $animals) {
+    echo "
+        <p>" . $key . $animals['name'] . $animals['category'] . $animals['birthday'] . "</p>";
+}
 ?>
+</div>
 </main>
 <footer><?php include "./includes/footer.php" ?></footer>
 </body>
