f23653661b3de82d054de9c1c929438e5a2b7758
Noel Andersson <noel.anderssonkohler@hotmail.com>
2021-04-06 11:57:16 +0200

    changed to priyas prevoius db-settings without ,

--- includes/db.php
+++ includes/db.php
@@ -4,7 +4,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-$dbh = new PDO('mysql:host=localhost;dbname=zoo;charset=UTF8',"user1", "1234");
+$dbh = new PDO('mysql:host=localhost;dbname=zoo;port=8022;charset =UTF8',"zooAdmin", "jesus7","user1", "1234");
 $animalsQuery ="
 SELECT 
 animals.id,
