1217537e6a1301495cf6c6b7569a90495dbcecd5
Jon Rickard Yxelflod <rickard@yxelflod.se>
2021-04-07 10:36:49 +0200

    add animal images

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,6 @@
 
 echo '<ul id="nav">
   <li><a href="../index.php">Home</a></li>
-  <li><a href="../priya.php">Priya</a></li>
+  <!-- <li><a href="../priya.php">Priya</a></li> -->
   <li><a href="../file-upload.php">File Upload</a></li>
   </ul>';
