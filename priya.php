f62a73d03bf80a4f3c6c4fda1d130788a669f57b
PriyaChilaka <priyakolukuluri@gmail.com>
2021-04-07 09:33:02 +0200

    priyas commits

--- priya.php
+++ priya.php
@@ -1,3 +1,17 @@
+<?php
+// Slå på all felrapportering. Bra under utveckling, dåligt i produktion.
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$dbh = new PDO('mysql:host=localhost;dbname=zoo;charset=UTF8;port=3307', "user1", "1234");
+// Execute query
+//$animals = $dbh->query($query);
+
+$categoryQuery = "SELECT category FROM animals GROUP BY category";
+
+$categories = $dbh->query($categoryQuery);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,17 +24,18 @@
 <body>
 <header><?php include "./includes/header.php" ?></header>
 <main> <h1>Dropdown</h1>
-<form class="fullForm" enctype="multipart/form-data" action="db.php" method="POST">
+<form class="fullForm" enctype="multipart/form-data" action="priya.php" method="POST">
 <label> Dropdownlist Animals:</label>
-<select name="animalNamn" id="name">
-<option value="">Choose a animal</option>
-<option value="Älg">Älg</option>
-<option value="Grå Jako">Grå Jako</option>
-<option value="Gädda">Gädda</option>
-<option value="Mört">Mört</option>
-<option value="Tordyvel">Tordyvel</option>
+ <select name="category">
+        <option value="">All</option>
+        <?php
+        foreach ($categories as $category) {
+          echo "<option value='" . $category['category'] . "'>" . $category['category'] . "</option>";
+        }
+        ?>
+      
 </select><br>
-<input type="submit" name="submit" value="select">
+
 </form>
 </main>
 <footer><?php include "./includes/footer.php" ?></footer>
